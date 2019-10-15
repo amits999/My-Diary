6bae7e324c105e249257dcd4379a995b38c7b342
amits999 <awesomeamit50@gmail.com>
2019-10-15 21:40:16 +0530

    Added functionality to login page

--- app/src/main/java/com/amit/action/my_diary/LoginActivity.java
+++ app/src/main/java/com/amit/action/my_diary/LoginActivity.java
@@ -1,13 +1,115 @@
 package com.amit.action.my_diary;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText email, password;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
+    private ImageView googleLogin, facebookLogin, githubLogin, twitterLogin;
+    private ProgressDialog mProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        email=findViewById(R.id.login_email_field);
+        password=findViewById(R.id.login_password_field);
+        loginButton=findViewById(R.id.login_button);
+        googleLogin=findViewById(R.id.google_signin_button);
+        facebookLogin=findViewById(R.id.facebook_signin_button);
+        githubLogin=findViewById(R.id.facebook_signin_button);
+        twitterLogin=findViewById(R.id.twitter_signin_button);
+        mProgress= new ProgressDialog(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyDetails();
+            }
+        });
+    }
+
+    private void verifyDetails() {
+        String emailId=email.getText().toString().trim();
+        String pass=password.getText().toString().trim();
+
+        if (emailId.isEmpty()){
+            email.setError("Email id required!");
+            email.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailId).matches()){
+            email.setError("Enter a valid email address!");
+            email.requestFocus();
+            return;
+        }
+
+        if (pass.isEmpty()){
+            password.setError("Password should not be empty");
+            password.requestFocus();
+            return;
+        }
+
+        if (pass.length()<6){
+            password.setError("Password length should be 6");
+            password.requestFocus();
+            return;
+        }
+
+        loginUser(emailId,pass);
+    }
+
+    private void loginUser(String emailId, String pass) {
+
+        mProgress.setTitle("Logging In...");
+        mProgress.setCanceledOnTouchOutside(false);
+        mProgress.show();
+
+        mAuth.signInWithEmailAndPassword(emailId,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    mProgress.dismiss();
+                    Toast.makeText(LoginActivity.this, "Authentication Successful!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent =new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    mProgress.dismiss();
+
+                    if (task.getException() instanceof FirebaseAuthInvalidUserException){
+                        Toast.makeText(getApplicationContext(),"User with email address does not exist!",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
