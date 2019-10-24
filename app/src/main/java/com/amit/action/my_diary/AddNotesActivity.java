e5016338d717c212a036be0c742d1a86dbce6bad
amits999 <awesomeamit50@gmail.com>
2019-10-24 23:06:58 +0530

    Notes Layout

--- app/src/main/java/com/amit/action/my_diary/AddNotesActivity.java
+++ app/src/main/java/com/amit/action/my_diary/AddNotesActivity.java
@@ -3,9 +3,10 @@ package com.amit.action.my_diary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class AddNotesActivity extends AppCompatActivity {
-    private Toolbar mToolbar;
+    private Toolbar mToolbar,bottomToolbar;
 
 
     @Override
@@ -14,6 +15,8 @@ public class AddNotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_notes);
 
         mToolbar=findViewById(R.id.add_notes_bar);
+        bottomToolbar=findViewById(R.id.bottomAppBar);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle(null);
 
@@ -25,5 +28,13 @@ public class AddNotesActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.add_down_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
