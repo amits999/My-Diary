c960b8149a14e203245b00a6ccae1f231ad3ac8d
amits999 <awesomeamit50@gmail.com>
2019-12-04 22:21:10 +0530

    Added new activity Selected App.

---
+++ app/src/main/java/com/amit/action/my_diary/SelectedBooks.java
@@ -0,0 +1,14 @@
+package com.amit.action.my_diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_books);
+    }
+}
